cf0a3a98cc746408a17c61767e0c888b4923e668
Calex8012 <42974499+Calex8012@users.noreply.github.com>
2019-05-30 22:18:25 -0500

    prueba de commit

---
+++ Practicai2c.c
@@ -0,0 +1,28 @@
+#include <18F4620.h>
+#fuses HS, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
+#fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
+#use delay(clock=16000000)
+#use I2C(Master,Fast,sda=PIN_C4,scl=PIN_C3)
+
+#define RTC_DS1307_WRITE_ADDRESS 0xD0
+#define RTC_DS1307_READ_ADDRESS 0xD1
+#define DIRECCION_INICIO_LECTURA_DATOS 0X00
+#define ACK 0
+
+#define __DEBUG_SERIAL__ //Si comentas esta linea se deshabilita el debug por serial y el PIN_C6 puede ser usado en forma digital I/O
+
+#ifdef __DEBUG_SERIAL__
+   #define TX_232        PIN_C6
+   #use RS232(BAUD=9600, XMIT=TX_232, BITS=8,PARITY=N, STOP=1)
+   #use fast_io(c)
+#endif
+
+void main (void){
+   setup_oscillator(OSC_16MHZ);
+#ifdef __DEBUG_SERIAL__ //Deberiamos de proteger nuestras depuraciones de esta forma o usar una macro ya protegida.
+   printf("Hola Mundo\n");//Puedes usar putc o printf. Revisa la documentación de CCS para ver que mas puedes hacer.
+#endif
+   while(1){
+      
+   }
+}
